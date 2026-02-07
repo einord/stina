abece978d870da571fa84ec434eb9b36ab548d17
Jonatan Nyqvist <jonatan.nyqvist@consid.se>
2026-02-07 20:55:32 +0100

    feat: refactor Select and TextInput components to use new input structure and improve event handling

--- packages/ui-vue/src/components/extension-components/TextInput.vue
+++ packages/ui-vue/src/components/extension-components/TextInput.vue
@@ -4,6 +4,7 @@ import type { StyleValue } from 'vue'
 import { computed } from 'vue'
 import { tryUseExtensionContext } from '../../composables/useExtensionContext.js'
 import { useExtensionScope } from '../../composables/useExtensionScope.js'
+import TextInput from '../inputs/TextInput.vue'
 
 const props = defineProps<TextInputProps>()
 
@@ -11,14 +12,14 @@ const rootStyle = computed(() => props.style as StyleValue)
 const context = tryUseExtensionContext()
 const scope = useExtensionScope()
 
-async function handleInput(event: Event) {
-  const value = (event.target as HTMLInputElement).value
+async function handleInput(value: string) {
   if (context && props.onChangeAction) {
     try {
       // Merge the input value into the action params
-      const actionRef = typeof props.onChangeAction === 'string'
-        ? { action: props.onChangeAction, params: { value } }
-        : { ...props.onChangeAction, params: { ...props.onChangeAction.params, value } }
+      const actionRef =
+        typeof props.onChangeAction === 'string'
+          ? { action: props.onChangeAction, params: { value } }
+          : { ...props.onChangeAction, params: { ...props.onChangeAction.params, value } }
       await context.executeAction(actionRef, scope.value)
     } catch (error) {
       console.error('Failed to execute text input action:', error)
@@ -28,13 +29,12 @@ async function handleInput(event: Event) {
 </script>
 
 <template>
-  <label :style="rootStyle">
-    {{ props.label }}
-    <input
-      type="text"
-      :placeholder="props.placeholder"
-      :value="props.value"
-      @input="handleInput"
-    />
-  </label>
+  <TextInput
+    :model-value="props.value"
+    :label="props.label"
+    :placeholder="props.placeholder"
+    :hint="props.placeholder"
+    :style="rootStyle"
+    @update:model-value="handleInput"
+  />
 </template>
